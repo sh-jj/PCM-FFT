7701dd1c5e464d412c94b8bce69b25947f22a4c5
sh-jj <shjjhszs@gmail.com>
2017-07-18 13:43:45 +0800

    pcm-fft

--- src/pcmfft/PcmFFT.java
+++ src/pcmfft/PcmFFT.java
@@ -1,23 +1,11 @@
-/**
- * 
- */
+
 package pcmfft;
 
 import java.io.*;
 
-/**
- * @author john
- *
- */
 public class PcmFFT {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
 		int frequency = 44100;
 		try {
 		File file = new File("reverseme.pcm");
@@ -30,14 +18,26 @@ public class PcmFFT {
             for (int i = 0; i<buffer1.length;i++){
                 buffer1[i] = inputStream_org.readShort();
 			}
-				
+		inputStream_org.close();
 			double[][] soundRI = new double[][]{
 				Util.soundToDoubleArray(buffer1),new double[powerN]
 			};
 			
             Util.fft(soundRI);
+            
+            FileWriter  out = new FileWriter(new File("frequency.txt"));
+            System.out.print(soundRI[0][0]);
+            System.out.print(' ');
+            System.out.print(soundRI[1][0]);
+            for (int i=0; i <soundRI[0].length;i++)
+            {
+            	out.write(String.valueOf(soundRI[0][i])+' '+String.valueOf(soundRI[1][i])+'\n');
+            }
+            out.close();
 		}
-		catch (IOException e) {}
+		catch (IOException e) {
+			System.out.print("ayayayayayayayayaayayay");
+		}
 	}
 
 }
