a1d01519f2e1060c866dd0f0b3006734d9ecdcb8
sh-jj <shjjhszs@gmail.com>
2017-07-18 13:59:37 +0800

    pcm-FFT

--- src/pcmfft/PcmFFT.java
+++ src/pcmfft/PcmFFT.java
@@ -8,7 +8,9 @@ public class PcmFFT {
 	public static void main(String[] args) {
 		int frequency = 44100;
 		try {
-		File file = new File("reverseme.pcm");
+		//File file = new File("reverseme.pcm");
+
+		File file = new File("reverseme_recorded.pcm");
 		
         DataInputStream inputStream_org = new DataInputStream(new FileInputStream(file));
         int soundN = frequency;
@@ -25,13 +27,17 @@ public class PcmFFT {
 			
             Util.fft(soundRI);
             
-            FileWriter  out = new FileWriter(new File("frequency.txt"));
+            //FileWriter  out = new FileWriter(new File("frequency.txt"));
+
+            FileWriter  out = new FileWriter(new File("frequency_recorded.txt"));
+            
             System.out.print(soundRI[0][0]);
             System.out.print(' ');
             System.out.print(soundRI[1][0]);
             for (int i=0; i <soundRI[0].length;i++)
             {
-            	out.write(String.valueOf(soundRI[0][i])+' '+String.valueOf(soundRI[1][i])+'\n');
+            	out.write(String.valueOf(soundRI[0][i])+'\n');
+            	//out.write(String.valueOf(soundRI[0][i])+' '+String.valueOf(soundRI[1][i])+'\n');
             }
             out.close();
 		}
